a1110c6226ea0f0e8267c463059ea6b5ff1cf0e7
CloudiosV <cloudiosss@gmail.com>
2024-08-30 14:55:36 +0700

    made drawing but not smooth yet

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 let board = document.getElementById("board");
 let ctx = board.getContext("2d");
-// board.width = 1280;
-// board.height = 600;
+board.width = 800;
+board.height = 400;
+let mouseDown = false;
+
+window.onload = function(){ 
+    drawCanvas();
+    startDrawing();
+}
+
+function drawCanvas(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, board.width, board.height);
+}
+
+function cvsMouseDown(){
+    mouseDown = true;
+    console.log("down")
+}
+
+function cvsMouseMove(e){
+    if(mouseDown){
+        ctx.beginPath();
+
+        ctx.fillStyle = "black";
+        ctx.arc(e.offsetX, e.offsetY, 5, 0, 2 * Math.PI)
+        ctx.fill();
+
+        ctx.closePath();
+    }
+}
+
+function cvsMouseUp(){
+    mouseDown = false;
+    console.log("up")
+}
+
+function startDrawing(){
+    cvsMouseOver();
+    requestAnimationFrame(startDrawing);
+}
